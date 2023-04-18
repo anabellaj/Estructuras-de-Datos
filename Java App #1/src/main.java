5a3476fc3bbeb2b2e04847f42f04524b3f69490e
anabellajm <j.anabella@correo.unimet.edu.ve>
2023-04-17 22:27:04 -0400

    :)

---
+++ Java App #1/src/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Anabella Jaua
+ */
+public class main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
