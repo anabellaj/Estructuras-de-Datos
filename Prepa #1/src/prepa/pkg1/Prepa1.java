44bab12256b4d37fc2150e68c5a16520d0cbde83
anabellajm <j.anabella@correo.unimet.edu.ve>
2023-04-24 08:50:29 -0400

    :)

--- Prepa #1/src/prepa/pkg1/Prepa1.java
+++ Prepa #1/src/prepa/pkg1/Prepa1.java
@@ -16,22 +16,62 @@ public class Prepa1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        Castear variables
-        String numero ="190";
-        int numero1 = Integer.parseInt(numero);
-        System.out.println(numero1);
+////        Castear variables
+//        String numero ="190";
+//        int numero1 = Integer.parseInt(numero);
+//        System.out.println(numero1);
         
-//      Para los float hay que colocar float x =19.5f o float x = float(19.5)
+////      Para los float hay que colocar float x =19.5f o float x = float(19.5)
+//
+////      Seleccionar caracteres por indice:
+//        String j = "Hola";
+//        char h = j.charAt(0);
+//        System.out.println(h);
+////      Si queremos declarar un caracter, se debe realizar con comillas simples
+//
+////      Ejemplo de pop-up (parecido a scanner)
+//        String cadena = JOptionPane.showInputDialog("Frase");
+//        JOptionPane.showMessageDialog(null, cadena);
+        
+////      Hacer inputs 
+//        int x = Integer.parseInt(JOptionPane.showInputDialog("Numero 1"));
+//        int y = Integer.parseInt(JOptionPane.showInputDialog("Numero 2"));
+//        
+//        //En este caso se le incrementa 1 a la x unicamente, y se covnvierte en el valor inicial de x
+//        y=x++;
+//        
+//        JOptionPane.showMessageDialog(null, x);
+//        JOptionPane.showMessageDialog(null, y);
 
-//      Seleccionar caracteres por indice:
-        String j = "Hola";
-        char h = j.charAt(0);
-        System.out.println(h);
-//      Si queremos declarar un caracter, se debe realizar con comillas simples
+////        Condicional ternario
+//          int edad = 10;
+//          String mayorDeEdad = edad >= 18 ? "Es mayor de edad": "Es menor de edad";
+//          System.out.println(mayorDeEdad);
 
-//      Ejemplo de pop-up (parecido a scanner)
-        String cadena = JOptionPane.showInputDialog("Frase");
-        JOptionPane.showMessageDialog(null, cadena);
+////          Para condicionales con muchas condiciones, se utiliza SWITCH en vez de muchos else if
+//            int num = 1;
+//            switch (num){
+//                case 1: //Case 1 compara si num == 1 (lo que va despues de la palabra case)
+//                    System.out.println("El valor del num es 1");
+//                    break;
+//                case 2:
+//                    System.out.println("El valor del num es 2");
+//                    break;
+//                case 3:
+//                    System.out.println("El valor del num es 3");
+//                    break;
+//                default:
+//                    System.out.println("No es ninguno de los casos");
+
+//            }
+
+//          CICLOS
+    //      While loop
+//            int num = 0;
+//            while (num < 10){
+//                System.out.println(num);
+//                num++;
+//            }
     }
     
 }
