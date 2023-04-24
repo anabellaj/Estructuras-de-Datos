609b7027129f378c3ca1263ab17cc32168f27019
anabellajm <j.anabella@correo.unimet.edu.ve>
2023-04-24 01:15:29 -0400

    :)

--- Prepa #1/src/prepa/pkg1/Prepa1.java
+++ Prepa #1/src/prepa/pkg1/Prepa1.java
@@ -4,6 +4,8 @@
  */
 package prepa.pkg1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Anabella Jaua
@@ -14,7 +16,22 @@ public class Prepa1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+//        Castear variables
+        String numero ="190";
+        int numero1 = Integer.parseInt(numero);
+        System.out.println(numero1);
+        
+//      Para los float hay que colocar float x =19.5f o float x = float(19.5)
+
+//      Seleccionar caracteres por indice:
+        String j = "Hola";
+        char h = j.charAt(0);
+        System.out.println(h);
+//      Si queremos declarar un caracter, se debe realizar con comillas simples
+
+//      Ejemplo de pop-up (parecido a scanner)
+        String cadena = JOptionPane.showInputDialog("Frase");
+        JOptionPane.showMessageDialog(null, cadena);
     }
     
 }
