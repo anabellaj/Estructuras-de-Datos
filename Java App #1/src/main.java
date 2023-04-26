a5c028c4d4d7ba6070b72ef1f7398a8c2c08d49b
anabellajm <j.anabella@correo.unimet.edu.ve>
2023-04-26 12:27:33 -0400

    :)

--- Java App #1/src/main.java
+++ Java App #1/src/main.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -12,8 +15,24 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello beautiful world");
-        System.out.println("goodbye, please work");
-        System.out.println("YES")
+        System.out.println("h");
+        System.out.println("Por favor ingrese un numero: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int aux =  number-1;
+        boolean isPrime = true;
+        
+        while (aux > 1){
+            if (number%aux == 0){
+                isPrime = false;
+                break;
+            }
+            aux--;
+        }
+        if (isPrime){
+            System.out.println("Es primo");
+        } else{
+            System.out.println("No es primo");
+        }
     }
 }
